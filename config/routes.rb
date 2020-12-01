e275b1e8d9b59d97fed41fae9cc9b70a62cd24e2
Charles Proye <charles.proye@hec.edu>
2020-12-01 12:52:11 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :missions, only: [:index, :new, :create, :update, :edit ]
-  resources :habits, only: [:index, :new, :create, :update, :edit]
+  resources :missions, only: [:index, :new, :create, :update, :edit, :destroy ]
+  resources :habits, only: [:index, :new, :create, :update, :edit, :destroy]
 end
