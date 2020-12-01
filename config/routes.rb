5d21f6c4ea7f144deb7e16b577ee5ee87d3366bb
Pierre-Damien Caux <70229257+PierreDamienC@users.noreply.github.com>
2020-12-01 13:05:15 +0100

    Update routes.rb
    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :missions, only: [:index, :new, :create, :update, :edit, :destroy ]
-  resources :habits, only: [:index, :new, :create, :update, :edit, :destroy]
-  resources :missions, only: [:index, :new, :create, :update, :edit, :destroy ]
-  resources :habits, only: [:index, :new, :create, :update, :edit, :destroy]
-  resources :missions, only: [:index, :new, :create, :update, :edit, :destroy ]
-  resources :habits, only: [:index, :new, :create, :update, :edit, :destroy]
-  resources :missions, only: [:index, :new, :create, :update, :edit, :destroy ]
-  resources :habits, only: [:index, :new, :create, :update, :edit, :destroy]
+  devise_for :users do
+    resources :missions, only: [:index, :new, :create, :update, :edit, :destroy]
+    resources :habits, only: [:index, :new, :create, :update, :edit, :destroy]
+  end
+
+  resources :companies, param: :name, only: [:show] do
+    resources :libraries
+  end
+  
 end
