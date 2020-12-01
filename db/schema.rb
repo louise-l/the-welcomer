7272ba6c2ae8fcbefd3a55a4c106a1bd376b5259
Pierre Damien Caux <pierre-damien_caux@hotmail.fr>
2020-12-01 12:47:15 +0100

    add company to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_112806) do
+ActiveRecord::Schema.define(version: 2020_12_01_114354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,13 @@ ActiveRecord::Schema.define(version: 2020_12_01_112806) do
     t.string "job"
     t.string "role"
     t.text "description"
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "libraries", "companies"
   add_foreign_key "online_libraries", "companies"
+  add_foreign_key "users", "companies"
 end
