820fa8a848b4f34b67b89123979c1056a822c2d8
Charles Proye <charles.proye@hec.edu>
2020-12-02 18:02:20 +0100

    it is fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_12_02_145355) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,11 +67,6 @@ ActiveRecord::Schema.define(version: 2020_12_02_145355) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_missions_on_user_id"
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "team_members", force: :cascade do |t|
