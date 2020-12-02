680b6b10d2deea095e224c85cd0010c26b9a54e6
Pierre Damien Caux <pierre-damien_caux@hotmail.fr>
2020-12-02 17:06:07 +0100

    change domain

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.smtp_settings = {
-  domain: 'https://the-welcomer.herokuapp.com/',
+  domain: 'gmail.com',
   address:        "smtp.sendgrid.net",
   port:            587,
   authentication: :plain,
