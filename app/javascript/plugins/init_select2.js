291ae900a5b54a0467d33e85f4da6de723b61fbd
Charles Proye <charles.proye@hec.edu>
2020-12-09 15:54:25 +0100

    debug a class pb on team-mate navbar

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 import 'select2';
 
-// function definitions
 const initSelect2 = () => {
   const selector2 = document.querySelector('.js-example-basic-multiple')
   if (selector2) {
